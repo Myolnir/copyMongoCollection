dbf0b59d69f9323dc997d4dd4f66ff44e241d297
Myolnir <angel.muelas@gmail.com>
2019-06-28 13:17:46 +0200

    removed unnecesary dependencies

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const argv = require('minimist')(process.argv.slice(2));
-const makeLogger = require('denizen-lib-server/lib/utils/logger');
-const pino = require('pino');
 const config = require('./config');
-const logger = makeLogger(pino({
-  extreme: false,
-  name: 'OVER_PRO',
-  level: 'debug',
-}));
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
@@ -18,24 +11,23 @@ const collectionToCopy = (argv.c) ? argv.c : 'rates';
 
 async function main() {
   try {
-    logger.accessing(`Connecting to ${origin} database ...`);
+    console.log(`Connecting to ${origin} database ...`);
     const clientOrigin = await MongoClient.connect(config[origin].url, {});
-    logger.accessing(`Connecting to ${destination} database ...`);
+    console.log(`Connecting to ${destination} database ...`);
     const clientDestination = await MongoClient.connect(config[destination].url, {});
     const collectionOrigin = clientOrigin.db('hermesms').collection(collectionToCopy);
     const collectionDestination = clientDestination.db('hermesms').collection(collectionToCopy);
-    logger.accessing(`Removing collection in ${destination}...`);
+    console.log(`Removing collection in ${destination}...`);
     await collectionDestination.deleteMany({});
-    logger.accessing(`Getting collection from ${origin} ...`);
+    console.log(`Getting collection from ${origin} ...`);
     const collection = await collectionOrigin.find({}).toArray();
-    logger.accessing(`Inserting collection in  ${destination}...`);
+    console.log(`Inserting collection in  ${destination}...`);
     await collectionDestination.insertMany(collection);
     clientOrigin.close();
     clientDestination.close();
-    logger.end();
   }
   catch (err) {
-    logger.error({err}, 'Something went wrong ...');
+    console.log({err}, 'Something went wrong ...');
   }
 }
 
